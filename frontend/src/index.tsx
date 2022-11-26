5868c34be903caf232e9255e3720bef8ce415fb1
sreedhara-aneesh <sreedhara.aneesh@gmail.com>
2022-11-26 00:43:40 +0000

    add login/registration with google account

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,20 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.scss'
 import reportWebVitals from './reportWebVitals'
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQjigaQ9FS1UlSAeGwZmIkoWnv0AnqvEU",
+  authDomain: "flashcards-test-7fbf8.firebaseapp.com",
+  databaseURL: "https://flashcards-test-7fbf8-default-rtdb.firebaseio.com",
+  projectId: "flashcards-test-7fbf8",
+  storageBucket: "flashcards-test-7fbf8.appspot.com",
+  messagingSenderId: "506242405702",
+  appId: "1:506242405702:web:2e9349c19e3ed314704147",
+  measurementId: "G-5LBEP5LQTH"
+};
+
+const app = initializeApp(firebaseConfig);
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
